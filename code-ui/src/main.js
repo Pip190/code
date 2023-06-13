c5b5bc1b34b6013fad37805ed9bacfc697591b13
Pip190 <wjk152153431@outlook.com>
2023-06-14 00:11:59 +0800

    109集 搭建Vuex环境

--- code-ui/src/main.js
+++ code-ui/src/main.js
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// 引入store
+import store from "./store/index";
+// 关闭生产提示
+createApp().config.productionTip = false;
+createApp(store)
 createApp(App).mount('#app')
